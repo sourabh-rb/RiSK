4abaaa9330eb8676d7b3e4329e03e07d424ccbb9
Shivani <shivani.panwar@mail.concordia.ca>
2019-02-03 03:29:10 -0500

    Adding model classes

---
+++ models/Card.java
@@ -0,0 +1,31 @@
+
+/**
+ * This class represents the 3 types of cards that can be given to a player after each turn
+ * or after capturing the last country of another player.
+ * @author shivani
+ * @version 1.0
+ */
+public class Card {
+	
+	//Value of type of card : Infantry, Cavalry, Artillery
+	private String type;   
+
+/**
+ * This method gets the type of card that the player will receive.
+ * 
+ * @return type
+ */
+public String getType() {
+	return type;
+}
+
+
+/**
+ * This method defines the types of cards a player can own.
+ * 
+ * @param type Can be of 3 types: Infantry, Cavalry, Artillery
+ */
+public void setType(String type) {
+	this.type = type;
+}
+}
---
+++ models/Dice.java
@@ -0,0 +1,32 @@
+
+/**
+ * Class Dice represents the dice that are rolled during the battles in the game. The attacker and defender,
+ * both roll the dice and the player with higher value on the dice wins the round. The number of dice rolled
+ * by a player depends on the number of armies the attacking country has.
+ * 
+ * @author shivani
+ * @version 1.0
+ */
+public class Dice {
+//Value showed by the dice
+private int value;      
+
+/**
+ * This method gets the value of each dice roll a player makes.
+ * 
+ * @return value
+ */
+public int getValue() {
+	return value;
+}
+
+
+/**
+ * This method sets the value of each dice roll a player makes.
+ * 
+ * @param value ranges from 1 to 6
+ */
+public void setValue(int value) {
+	this.value = value;
+}
+}
